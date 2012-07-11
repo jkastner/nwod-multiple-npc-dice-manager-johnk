2b9b9b257f291a1932d752f85305bf091f4176a8
johnathan.kastner@gmail.com <johnathan.kastner@gmail.com@8665b73a-cc4a-6e25-d301-bb58aca6797e>
2012-07-11 20:24:29 +0000

    First commit.

---
+++ trunk/Trait.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XMLCharSheets
+{
+    public class Trait
+    {
+        private String _traitName;
+
+        public String TraitName
+        {
+            get { return _traitName; }
+            set { _traitName = value; }
+        }
+
+        public Trait(string traitName)
+        {
+            _traitName = traitName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Trait other = obj as Trait;
+            if (other == null)
+                return false;
+            return other.TraitName.Equals(TraitName);
+        }
+    }
+}
