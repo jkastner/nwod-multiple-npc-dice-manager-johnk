3ffebe575b50c91c08bd824c5ed8e40d49aa4d84
johnathan.kastner@gmail.com <johnathan.kastner@gmail.com@8665b73a-cc4a-6e25-d301-bb58aca6797e>
2012-07-12 05:25:05 +0000

    Added simple dice-roller.

---
+++ RollDice.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XMLCharSheets
+{
+    public class RollDice
+    {
+        static Random _theRandomGenerator = new Random();
+        private int _numberOfDice;
+
+        public int NumberOfDice
+        {
+            get { return _numberOfDice; }
+            set { _numberOfDice = value; }
+        }
+
+        private int _currentSuccesses;
+        public int CurrentSuccesses
+        {
+            get { return _currentSuccesses; }
+            set { _currentSuccesses = value; }
+        }
+
+        private String _resultDescription;
+        public String ResultDescription
+        {
+            get { return _resultDescription; }
+            set { _resultDescription = value; }
+        }
+
+
+
+        public RollDice(int diceNum)
+        {
+            _numberOfDice = diceNum;
+        }
+
+
+
+
+
+        int _maxSides = 11;
+        internal void Roll()
+        {
+            CurrentSuccesses = 0;
+            _resultDescription = "";
+            for (int i = 0; i < NumberOfDice; i++)
+            {
+                int result = _theRandomGenerator.Next(1, _maxSides);
+                if (String.IsNullOrEmpty(_resultDescription))
+                {
+                    _resultDescription = result.ToString();
+                }
+                else
+                {
+                    _resultDescription = _resultDescription + ", " + result.ToString();
+                }
+                if (CheckSuccess(result))
+                {
+                    CurrentSuccesses++;
+                }
+                while (result == _maxSides-1)
+                {
+                    result = _theRandomGenerator.Next(1, _maxSides);
+                    _resultDescription = _resultDescription+"->"+result.ToString();
+                    if (CheckSuccess(result))
+                    {
+                        CurrentSuccesses++;
+                    }
+                }
+            }
+        }
+
+        private bool CheckSuccess(int theResult)
+        {
+            return theResult > 7;
+        }
+    }
+}
