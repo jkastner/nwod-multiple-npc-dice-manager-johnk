4c72d3e6490151b5fc1702ee1e9fedba98eb9ccf
johnathan.kastner@gmail.com <johnathan.kastner@gmail.com@8665b73a-cc4a-6e25-d301-bb58aca6797e>
2012-07-23 21:35:08 +0000

    New commit with changes to handle single instead of multiple chars. Branching to keep work with multiple chars intact.

---
+++ SingleCharOnly/Trait.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XMLCharSheets
+{
+    public class Trait
+    {
+        private String _traitName;
+
+        public String TraitName
+        {
+            get { return _traitName; }
+            set { _traitName = value; }
+        }
+
+        public Trait(string traitName)
+        {
+            _traitName = traitName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Trait other = obj as Trait;
+            if (other == null)
+                return false;
+            return other.TraitName.Equals(TraitName);
+        }
+
+        public override string ToString()
+        {
+            return _traitName;
+        }
+    }
+}
---
+++ SingleCharOnly/Traits.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace XMLCharSheets
+{
+    public class Traits : ObservableCollection<Trait>
+    {
+ 
+        
+        public Trait AddIfNew(String traitName)
+        {
+            Trait newTrait = new Trait(traitName);
+            if(!this.Contains(newTrait))
+            {
+                this.Add(newTrait);
+            }
+            else
+            {
+                newTrait = this[IndexOf(newTrait)];
+            }
+            return newTrait;
+        }
+    }
+}
