277d041dce7fac2211a978ea2fd6d2a7ef6f2bed
johnathan.kastner@gmail.com <johnathan.kastner@gmail.com@8665b73a-cc4a-6e25-d301-bb58aca6797e>
2013-08-14 03:30:55 +0000

    Further work on UI refactor.

--- trunk/CharacterCreationControl.xaml.cs
+++ trunk/CharacterCreationControl.xaml.cs
@@ -20,10 +20,83 @@ namespace XMLCharSheets
     /// </summary>
     public partial class CharacterCreationControl : UserControl
     {
+        CharacterCreationViewModel _characterCreationViewModel = new CharacterCreationViewModel();
         public CharacterCreationControl()
         {
             InitializeComponent();
+            DataContext = _characterCreationViewModel;
+            AvailableNPCS_ListBox.ItemsSource = _characterCreationViewModel.FilteredCharacters;
             
+            
+            
+            /// Take largely from 
+            /// http://www.intertech.com/Blog/post/How-to-Select-All-Text-in-a-WPF-Content-on-Focus.aspx
+            EventManager.RegisterClassHandler(typeof(TextBox), UIElement.PreviewMouseLeftButtonDownEvent,
+                new MouseButtonEventHandler(SelectivelyHandleMouseButton), true);
+            EventManager.RegisterClassHandler(typeof(TextBox), UIElement.GotKeyboardFocusEvent,
+                new RoutedEventHandler(SelectAllText), true);
+            ///
+        }
+
+
+
+
+
+        /// <summary>
+        /// Take largely from 
+        /// http://www.intertech.com/Blog/post/How-to-Select-All-Text-in-a-WPF-Content-on-Focus.aspx
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void SelectivelyHandleMouseButton(object sender, MouseButtonEventArgs e)
+        {
+            var textbox = (sender as TextBox);
+            if (textbox != null && !textbox.IsKeyboardFocusWithin)
+            {
+                if (e.OriginalSource.GetType().Name == "TextBoxView")
+                {
+                    e.Handled = true;
+                    textbox.Focus();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Take largely from 
+        /// http://www.intertech.com/Blog/post/How-to-Select-All-Text-in-a-WPF-Content-on-Focus.aspx
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void SelectAllText(object sender, RoutedEventArgs e)
+        {
+            var textBox = e.OriginalSource as TextBox;
+            if (textBox != null)
+                textBox.SelectAll();
+        }
+
+        private void CharacterSheetSearcher_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RunSearch();
+        }
+
+        private void RunSearch()
+        {
+            var searchText = CharacterSheetSearcher_TextBox.Text.Trim().ToLower();
+            if(searchText.Equals("search..."))
+                return;
+            ImageSearch_TextBox.Text = CharacterSheetSearcher_TextBox.Text;
+            _characterCreationViewModel.SearchForText(searchText);
+            if (_characterCreationViewModel.FilteredCharacters.Count >= 1)
+            {
+                AvailableNPCS_ListBox.SelectedIndex = 0;
+            }
+
+        }
+
+        private void CharacterCreationControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            _characterCreationViewModel.ResetActiveList();
+            RunSearch();
         }
 
 
---
+++ trunk/CharacterCreationViewModel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLCharSheets
+{
+    internal class CharacterCreationViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<CharacterSheet> FilteredCharacters = new ObservableCollection<CharacterSheet>();
+        public CharacterCreationViewModel()
+        {
+            ResetActiveList();
+        }
+        public CharacterSheet _selectedNewCharacter;
+        public CharacterSheet SelectedNewCharacter
+        {
+            get { return _selectedNewCharacter; }
+            set 
+            { 
+                _selectedNewCharacter = value;
+                OnPropertyChanged("SelectedNewCharacter");
+                CheckForNameChange();
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+
+
+        #endregion
+
+        internal void SearchForText(string targetCharacterSheet)
+        {
+            if (String.IsNullOrWhiteSpace(targetCharacterSheet))
+            {
+                ResetActiveList();
+                return;
+            }
+            FilteredCharacters.Clear();
+            var matchingList = CombatService.RosterViewModel.FullRoster.Where(x => x.Name.ToLower().Contains(targetCharacterSheet));
+            foreach (var cur in matchingList)
+            {
+                FilteredCharacters.Add(cur);
+            }
+        }
+
+        public void ResetActiveList()
+        {
+            if (CombatService.RosterViewModel.FullRoster.Count() != FilteredCharacters.Count())
+            {
+                FilteredCharacters.Clear();
+                foreach (var cur in CombatService.RosterViewModel.FullRoster)
+                {
+                    FilteredCharacters.Add(cur);
+                }
+            }
+        }
+        private String _currentCharacterName = "Name";
+        public string CurrentCharacterName
+        {
+            get
+            {
+                if (SelectedNewCharacter == null)
+                {
+                    return _currentCharacterName;
+                }
+                return _currentCharacterName;
+            }
+            set 
+            {
+                _currentCharacterName = value;
+                OnPropertyChanged("CurrentCharacterName");
+            }
+        }
+
+        private void CheckForNameChange()
+        {
+            
+            //Set the current name to the default character name if it equals 'name' or if it's empty or if the name matches an existing character name.
+            if (CurrentCharacterName.Equals("Name")||
+                String.IsNullOrWhiteSpace(CurrentCharacterName.Trim()) ||
+                CombatService.RosterViewModel.FullRoster.Any(x => x.Name.Equals(CurrentCharacterName)))
+            {
+                if (SelectedNewCharacter == null)
+                {
+                    CurrentCharacterName = "Name";
+                }
+                else
+                {
+                    CurrentCharacterName = SelectedNewCharacter.Name;
+                }
+            }
+        }
+
+    }
+}
