683fa863c2762ae49a92425c0ba2553d315739dc
johnathan.kastner@gmail.com <johnathan.kastner@gmail.com@8665b73a-cc4a-6e25-d301-bb58aca6797e>
2013-10-29 03:43:14 +0000

    Removing pictures, added handling of map background.

---
+++ GameBoard/GameBoard/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBoard
+{
+    public class Constants
+    {
+        public const string MapPictureDirectory = "MapPictures";
+        public const string BackgroundName = "MapBackground.jpg";
+    }
+}
