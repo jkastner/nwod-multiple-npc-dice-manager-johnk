ad081fd43688e35b76113ff1174e6d1a4291b4e8
johnathan.kastner@gmail.com <johnathan.kastner@gmail.com@8665b73a-cc4a-6e25-d301-bb58aca6797e>
2013-11-12 04:11:20 +0000

    Added textbox input handling and enforced some pathfinder saving throw rules

---
+++ PositiveIntegerTextBox.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XMLCharSheets
+{
+    /// <summary>
+    /// Interaction logic for PositiveIntegerTextBox.xaml
+    /// </summary>
+    public partial class PositiveIntegerTextBox : TextBox
+    {
+        public PositiveIntegerTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private static bool IsTextNumeric(string text)
+        {
+            text = text.Trim();
+            var regex = new Regex("[^0-9]"); //regex that matches disallowed text
+            bool isGood = !regex.IsMatch(text);
+            return isGood;
+        }
+
+        private void PositiveIntegerTextBox_TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            bool isGood = IsTextNumeric(e.Text);
+            e.Handled = !isGood;
+        }
+
+        // Use the DataObject.Pasting Handler 
+        private void PositiveIntegerTextBox_TextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(String)))
+            {
+                var text = (String)e.DataObject.GetData(typeof(String));
+                if (!IsTextNumeric(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
+    }
+}
